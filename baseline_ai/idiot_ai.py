77c82646afd1a9a3dec81af8ae843539ef547ece
zhes2Hen <138995133+zhes2Hen@users.noreply.github.com>
2024-05-06 18:15:07 +0800

    change api_description, gambler bot and idiot bot

--- baseline_ai/idiot_ai.py
+++ baseline_ai/idiot_ai.py
@@ -2,15 +2,6 @@ import realm
 import random
 
 
-def get_all_actions(layout, side):
-    # 辅助函数，用于给出所有可行行动
-    list_actions = [None]
-    for id_ in [realm.ChessType.WARRIOR, realm.ChessType.ARCHER, realm.ChessType.PROTECTOR]:
-        list_actions.extend(realm.get_valid_actions(layout, side, id_)[1:])
-    return list_actions
-
-
 @realm.api_decorator
 def update(board):
-    my_side = board.my_side
-    return random.choice(get_all_actions(board.layout, my_side))
+    return random.choice(realm.get_valid_actions(board.layout, board.my_side))
