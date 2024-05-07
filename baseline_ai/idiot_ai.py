ad28f5c47629a7a8332016f70588235c99371cd9
zhes2Hen <138995133+zhes2Hen@users.noreply.github.com>
2024-05-07 14:50:37 +0800

    add simple copyright info

--- baseline_ai/idiot_ai.py
+++ baseline_ai/idiot_ai.py
@@ -1,3 +1,12 @@
+# 此文件版权归pkulab409/dsa2024dev全体技术组同学所有
+# The copyright of this file belongs to all technical team members of pkulab409/dsa2024dev
+# https://github.com/orgs/pkulab409/teams/dsa2024dev
+#
+# 此文件归属于项目pkudsa.realm
+# This file belongs to the project pkudsa.realm
+# https://github.com/pkulab409/pkudsa.realm
+#
+# 特别感谢CrystalZZZZZZZZ和zhes2Hen在编写上做出的贡献
 import realm
 import random
 
